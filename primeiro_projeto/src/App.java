7ae683d011804dd95a8fcdf486ddbf65c1ff35ee
renato696 <renato696@gmail.com>
2024-08-16 14:09:38 -0300

    classes

--- primeiro_projeto/src/App.java
+++ primeiro_projeto/src/App.java
@@ -27,3 +27,16 @@ public class App {
         System.out.println(classificacao);
     }
 }
+/* 
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lançamento que os clientes estão curtindo");
+        }
+    }
+}
+*/
