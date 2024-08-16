bea0801008e3b265f709fc05b2b74271a6bac25e
renato696 <renato696@gmail.com>
2024-08-16 11:22:31 -0300

    variaveis

--- primeiro_projeto/src/App.java
+++ primeiro_projeto/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: TopGun: Maverick");
+
+        int ano;
+        ano = 2022;
     }
 }
