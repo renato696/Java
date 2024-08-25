af49adc6484f2f3b0405f5df496c4b1c560fa8c6
renato696 <renato696@gmail.com>
2024-08-25 18:14:43 -0300

    menu

--- Alura/contaBancaria/src/App.java
+++ Alura/contaBancaria/src/App.java
@@ -1,13 +1,39 @@
+
+import java.util.Scanner;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        String nome ="Renato Pires";
-        String tipoConta = "Corrente";
-        double saldo = 1599.99;
+        String name ="Renato Pires";
+        String accountType = "Corrente";
+        double accountBalance = 1599.99;
 
         System.out.println("********************************");
-        System.out.println("\nNome do cliente: " + nome);
-        System.out.println("Tipo de conta: " + tipoConta);
-        System.out.println("Saldo: R$" + saldo);
+        System.out.println("\nNome do cliente: " + name);
+        System.out.println("Tipo de conta: " + accountType);
+        System.out.println("Saldo: R$" + accountBalance);
         System.out.println("\n********************************");
+
+        String menu = """
+                **Digite a opção desejada:
+                
+                1- Consultar saldo
+                2- Transferir valor
+                3- Receber valor
+                4- Sair
+                
+                Opção:
+                """;
+
+        Scanner reading = new Scanner (System.in);
+
+
+        int operation = 0;
+        while (operation != 4) {
+            System.out.println(menu);
+            operation = reading.nextInt();
+        }
+
     }
 }
