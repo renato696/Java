e0c8da84815ee3fa574aa8467096afeb08c6a1a5
renato696 <renato696@gmail.com>
2024-08-25 13:56:49 -0300

    loop - for

--- primeiro_projeto/src/Loop.java
+++ primeiro_projeto/src/Loop.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Loop {
     public static void main(String[] args) {
-        
+        Scanner reading = new Scanner(System.in);
+        double avaliacaoMedia = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Qual sua avaliação para o filme?");
+            nota = reading.nextDouble();
+            avaliacaoMedia += nota;
+        }
+
+        System.out.println("Média de avaliação: " + avaliacaoMedia/3);
+
     }
 }
