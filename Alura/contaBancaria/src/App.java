166005facf150baadb7ff88edfbb7019a48ce328
renato696 <renato696@gmail.com>
2024-08-25 17:53:34 -0300

    dados do cliente

--- Alura/contaBancaria/src/App.java
+++ Alura/contaBancaria/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome ="Renato Pires";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("********************************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo: R$" + saldo);
+        System.out.println("\n********************************");
     }
 }
