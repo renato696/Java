3cd50507d6bc08375dd0d760932b36bd94bd5392
renato696 <renato696@gmail.com>
2024-08-26 09:28:37 -0300

    classes - atributos

---
+++ Alura/screenMatch/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        Movie myMovie = new Movie();
+        myMovie.name = "Godfather";
+        myMovie.releaseYear = 1970;
+        myMovie.movieLength = 180;
+
+        System.out.println(myMovie.name);
+    }
+}
