d857811e7c47ed30de34afae443dbf5bd90f9523
renato696 <renato696@gmail.com>
2024-08-28 08:28:34 -0300

    movie update with subclass attributes and methods

--- Alura/screenMatch/src/br/com/alura/screenmatch/models/Movie.java
+++ Alura/screenMatch/src/br/com/alura/screenmatch/models/Movie.java
@@ -2,5 +2,14 @@ package br.com.alura.screenmatch.models;
 
 public class Movie {
 
+    private String director;
+
+    public String getDirector(){
+        return director;
+    }
+
+    public void setDirector(String director){
+        this.director = director;
+    }
 
 }
