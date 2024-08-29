934900d2ddfc6c24e1688465958b9cce30f8b36c
renato696 <renato696@gmail.com>
2024-08-29 10:36:13 -0300

    Recomendations - method

--- Alura/screenMatch/src/br/com/alura/screenmatch/calculations/Recomendations.java
+++ Alura/screenMatch/src/br/com/alura/screenmatch/calculations/Recomendations.java
@@ -1,5 +1,14 @@
 package br.com.alura.screenmatch.calculations;
 
 public class Recomendations {
-
+    
+    public void recomendationsFilter(Classification classification){
+        if(classification.getClassification() >= 4){
+            System.out.println("Está entre os preferidos do momento!");
+        } else if (classification.getClassification() >= 2){
+            System.out.println("Muito bem avaliado no momento!");
+        } else {
+            System.out.println("Inclua na sua lista para assistir depois!");
+        }
+    }
 }
