6053d7e89ce033c921b8bba842a056f356f1e5b0
renato696 <renato696@gmail.com>
2024-08-29 09:33:03 -0300

    episode - attributes, getters and setters

--- Alura/screenMatch/src/br/com/alura/screenmatch/models/Episode.java
+++ Alura/screenMatch/src/br/com/alura/screenmatch/models/Episode.java
@@ -1,5 +1,27 @@
 package br.com.alura.screenmatch.models;
 
 public class Episode {
+    private int episodeNumber;
+    private int episodeName;
+    private Series serie;
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
+    public void setEpisodeNumber(int episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+    public int getEpisodeName() {
+        return episodeName;
+    }
+    public void setEpisodeName(int episodeName) {
+        this.episodeName = episodeName;
+    }
+    public Series getSerie() {
+        return serie;
+    }
+    public void setSerie(Series serie) {
+        this.serie = serie;
+    }
 
 }
