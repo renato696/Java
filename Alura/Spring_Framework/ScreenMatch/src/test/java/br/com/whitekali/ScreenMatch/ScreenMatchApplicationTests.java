6a58620f87813b1e0708b030de125e4b10d8b0a8
renato696 <renato696@gmail.com>
2024-10-11 11:35:29 -0300

    spring boot starting project

---
+++ Alura/Spring_Framework/ScreenMatch/src/test/java/br/com/whitekali/ScreenMatch/ScreenMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.whitekali.ScreenMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
