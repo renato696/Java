e327cf52d361ee06ab017ff34eedb5531cea1270
renato696 <renato696@gmail.com>
2024-10-11 15:06:02 -0300

    consumindo API

--- Alura/Spring_Framework/ScreenMatch/src/main/java/br/com/whitekali/ScreenMatch/ScreenMatchApplication.java
+++ Alura/Spring_Framework/ScreenMatch/src/main/java/br/com/whitekali/ScreenMatch/ScreenMatchApplication.java
@@ -1,13 +1,22 @@
 package br.com.whitekali.ScreenMatch;
 
+import br.com.whitekali.ScreenMatch.service.ConsumiAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatchApplication {
+public class ScreenMatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumiAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
 	}
 
 }
