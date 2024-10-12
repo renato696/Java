916f65d9a7a16cd04660b5719b1e3697f9c1519b
renato696 <renato696@gmail.com>
2024-10-12 10:35:39 -0300

    water pumps

--- Projeto_Extensao/Condominio_4_0/src/Pumps.java
+++ Projeto_Extensao/Condominio_4_0/src/Pumps.java
@@ -1,3 +1,22 @@
-public class Pumps extends App{
+public class Pumps extends Sensors{
 
+    private int water_pump1;
+    private int water_pump2;
+
+    public void setWater_pump1(){
+        water_pump1 = getPump1();
+    }
+
+    public void setWater_pump2(){
+        water_pump1 = getPump1();
+    }
+
+
+
+
+
+
+
+
+    
 }
--- Projeto_Extensao/Condominio_4_0/src/Sensors.java
+++ Projeto_Extensao/Condominio_4_0/src/Sensors.java
@@ -5,6 +5,9 @@ public class Sensors {
     private int sensor3 = 1;
     private int water_pipe = 1;
 
+    private int pump1 = 1;
+    private int pump2 = 1;
+
 
     public int getSensor1() {
         return sensor1;
@@ -22,10 +25,21 @@ public class Sensors {
         return water_pipe;
     }
 
+    public int getPump1() {
+        return pump1;
+    }
+    
+    public int getPump2() {
+        return pump2;
+    }
+
+
     public void startSensors(){
         System.out.println(String.format("Sensor 1 status: %d", sensor1));
         System.out.println(String.format("Sensor 2 status: %d", sensor2));
         System.out.println(String.format("Sensor 3 status: %d", sensor3));
         System.out.println(String.format("Sensor flow status: %d", water_pipe));
+        System.out.println(String.format("Pump1 status %d", pump1));
+        System.out.println(String.format("Pump2 status %d", pump2));
     }
 }
